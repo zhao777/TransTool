ce18a934e159fb8d7da7b29cb8e05b2a1cd0bb79
zhao777 <35123482+zhao777@users.noreply.github.com>
2019-05-27 15:48:14 +0800

    add file
    add project

---
+++ ES_TransTool/ES_TransTool/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ES_TransTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(650, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "设置TS文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(631, 388);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(631, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(650, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "选择源文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.Location = new System.Drawing.Point(650, 52);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(95, 230);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "操作步骤：\r\n1、点击<选择源文件>按钮,选择翻译资源文件（EXCEL），如果有多个，可以依次选择\r\n2、选择源语言（Source）和目标语言（Target）\r\n" +
+    "3、点击<更新TS文件>按钮，选择您要设置的目标文件（TS）\r\n4、待补充";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(650, 358);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(95, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(649, 310);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(95, 20);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(648, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Source:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(648, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Target:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ES_TransTool_V1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ES_TransTool/ES_TransTool/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Data.OleDb;
+
+namespace ES_TransTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 保存从excel中读取的翻译源
+        /// </summary>
+        public Dictionary<string, string> m_dicSouce = new Dictionary<string, string>();
+        public DataTable dt = new DataTable();
+
+        /// <summary>
+        /// 根据字典内容设置ts文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //把datatable中的数据保存到m_dicSouce中
+            int nCount = dt.Rows.Count;
+            for (int i = 0; i < nCount; ++i)
+            {
+                string strKey = dt.Rows[i][comboBox2.SelectedIndex].ToString();
+                string strValue = dt.Rows[i][comboBox1.SelectedIndex].ToString();
+                m_dicSouce[strKey] = strValue;
+            }
+
+            int nSucessNum = 0;
+            //string xmlpath = System.AppDomain.CurrentDomain.BaseDirectory + "LocalMaintSystem_en.ts";
+            string xmlpath = string.Empty;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "TS文件(*.xml;*.ts)|*.xml;*.ts|所有文件|*.*";
+            ofd.ValidateNames = true;
+            ofd.CheckPathExists = true;
+            ofd.CheckFileExists = true;
+            ofd.Title = "请选择您要更新的TS文件";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                xmlpath = ofd.FileName;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlpath);
+            XmlNode xnTs = doc.SelectSingleNode("//TS");
+            XmlNodeList xnTsList = xnTs.ChildNodes;
+            foreach (XmlNode xnTsChild in xnTsList)
+            {
+                if (xnTsChild.Name == "context")
+                {
+                    XmlNodeList xnList = xnTsChild.ChildNodes;
+                    foreach (XmlNode xnf in xnList)
+                    {
+                        XmlElement xe = (XmlElement)xnf;
+                        if (xe.Name == "message")
+                        {
+                            XmlNodeList xnf1 = xe.ChildNodes;
+                            foreach (XmlNode xn2 in xnf1)
+                            {
+                                switch (comboBox2.SelectedIndex)
+                                {
+                                    case 0:
+                                        {
+                                            //用中文替换，使用source节点
+                                            if (xn2.Name.CompareTo("source") == 0)
+                                            {
+                                                string sourceText = xn2.InnerText;
+                                                XmlNode xnTranslation = xn2.NextSibling;
+                                                if (xnTranslation.Name == "translation")
+                                                {
+                                                    if (m_dicSouce.ContainsKey(sourceText))
+                                                    {
+                                                        //修改节点translation的值
+                                                        xnTranslation.InnerText = m_dicSouce[sourceText];
+                                                        nSucessNum++;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        break;
+                                    case 1:
+                                        {
+                                            //用英文替换，使用translation节点
+                                            if (xn2.Name.CompareTo("translation") == 0)
+                                            {
+                                                string translationText = xn2.InnerText;
+                                                if (m_dicSouce.ContainsKey(translationText))
+                                                {
+                                                    //修改节点translation的值
+                                                    xn2.InnerText = m_dicSouce[translationText];
+                                                    nSucessNum++;
+                                                }
+                                            }
+                                        }
+                                        break;
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+            doc.Save(xmlpath);
+            MessageBox.Show(nSucessNum.ToString() + "elements trans success!");
+        }
+
+
+        /// <summary>
+        /// 从excel中获取需要翻译的资源，保存到DataTable
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static DataTable ReadExcelToTable(string path)
+        {
+            try
+            {
+                string strConn;
+                // strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=False;IMEX=1'";
+                strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=\"Excel 8.0;HDR=YES\"";
+                OleDbConnection OleConn = new OleDbConnection(strConn);
+                OleConn.Open();
+                String sql = "SELECT * FROM  [Sheet1$]";//可是更改Sheet名称，比如sheet2，等等
+
+                OleDbDataAdapter OleDaExcel = new OleDbDataAdapter(sql, OleConn);
+                DataSet OleDsExcle = new DataSet();
+                OleDaExcel.Fill(OleDsExcle, "Sheet1");
+                OleConn.Close();
+
+                return OleDsExcle.Tables["Sheet1"];
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("数据绑定Excel失败!失败原因：" + err.Message, "提示信息",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// 选择翻译文件excel并加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel文件(*.xls;*.xlsx)|*.xls;*.xlsx|所有文件|*.*";
+            ofd.ValidateNames = true;
+            ofd.CheckPathExists = true;
+            ofd.CheckFileExists = true;
+            ofd.Title = "请选择翻译源文件（EXCEL）";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+
+                //加载excel
+                // DataTable dt;
+                dt = ReadExcelToTable(ofd.FileName);
+                dataGridView1.DataSource = dt;
+
+                //把第一行语言项加到列表
+                int nLanguageCound = dt.Columns.Count;
+                comboBox1.Items.Clear();
+                comboBox2.Items.Clear();
+                for (int i = 0; i < nLanguageCound; ++i)
+                {
+                    comboBox1.Items.Add(dt.Columns[i].ToString());
+                    comboBox2.Items.Add(dt.Columns[i].ToString());
+                }
+                comboBox1.SelectedIndex = 1;
+                comboBox2.SelectedIndex = 0;
+            }
+        }
+    }
+}
